262e735d21f5f5cb22982eb3240dc7dbf73bea2a
bahareeskandari <bahareeskandarijou@gmail.com>
2020-08-03 16:52:24 +0200

    everything working, now client side left

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,14 +1,12 @@
-const env = require('./env.js')
-
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: env.dialect,
+  dialect: 'postgres',
   operatorsAliases: false,
   pool: {
-    max: env.max,
-    min: env.pool.min,
-    acquire: env.pool.acquire,
-    idle: env.pool.idle,
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
   },
 })
 
