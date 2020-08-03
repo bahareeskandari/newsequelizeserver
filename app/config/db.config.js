add0b84c5f4b3500d1fa55ebe9cebd0f209f6e23
bahareeskandari <bahareeskandarijou@gmail.com>
2020-08-03 14:22:03 +0200

    trying with DATABASE_URL to test

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,26 +1,24 @@
-const env = require('./env.js');
- 
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(env.database, env.username, env.password, {
-  host: env.host,
+const env = require('./env.js')
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: env.dialect,
   operatorsAliases: false,
- 
+
   pool: {
     max: env.max,
     min: env.pool.min,
     acquire: env.pool.acquire,
-    idle: env.pool.idle
-  }
-});
- 
-const db = {};
- 
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
- 
+    idle: env.pool.idle,
+  },
+})
+
+const db = {}
+
+db.Sequelize = Sequelize
+db.sequelize = sequelize
+
 //Models/tables
-db.customers = require('../model/customer.model.js')(sequelize, Sequelize);
- 
- 
-module.exports = db;
+db.customers = require('../model/customer.model.js')(sequelize, Sequelize)
+
+module.exports = db
